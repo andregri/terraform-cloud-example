b8c5e84730ad8cf9dffec8ed1a39a575a631ee6c
Andrea Grillo <andrea.grillo96@live.com>
2023-03-31 11:28:33 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,7 @@
-variable "region" {
-  description = "region"
-  default     = "us-west-1"
+variable "sa" {
+  description = "service account"
+}
+
+variable "project_id" {
+  description = "gcp project id"
 }
