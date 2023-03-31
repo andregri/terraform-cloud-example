2103d5a83d535f63386ef6f025347ef8c64bd75d
Andrea Grillo <andrea.grillo96@live.com>
2023-03-31 12:01:43 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,6 +7,13 @@ terraform {
       name = "learn-terraform-cloud"
     }
   }
+  
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
 
   required_version = ">= 1.1.0"
 }
