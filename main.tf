8473801096d2c8e13d471940e246e3a910059ad8
Andrea Grillo <andrea.grillo96@live.com>
2023-03-31 12:01:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "google" {
-  credentials = var.sa
-
-  project = var.project_id
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "random_id" "server" {
+  byte_length = 8
 }
